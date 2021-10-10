5ea3352933e45a5a1191763d8c8db418c60041c8
Rufus Vijayaratnam <rufusvijayaratnam@gmail.com>
2021-10-10 10:09:39 +0100

    Is working as intended for the masking and reconstruction, am currently working on making it show the delta between each image

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import numpy as np
 import cv2 as cv
 
+"""
+WARNING: Do NOT run this more than once in iPython Kernel, for some
+reason you get slightly different results every time, which makes no sense
+I will fix it later, but I'm not really sure why it happens.
+Just navigate to directory and "python main.py"
+Or just straight up run it
+"""
+
+"""
+Future work: Also detect unique colours from actual image then compare
+to colours found in the colour scale and match them, this will result in
+perfect reconstruction without the little black lines everywhere
+"""
+
 im1 = cv.imread("im1.png")
 im2 = cv.imread("im2.png")
 
@@ -18,9 +32,11 @@ colours2 = np.unique(colour_scale2.reshape(-1, colour_scale2.shape[-1]), axis=0)
 #on the scale manually
 unique_colours = len(colours1)
 
+#Range of values represented by each colour on the colour scale
 vals = np.linspace(-1, 1.2, unique_colours)
 
 #Just for debugging
+############################################################
 def showim(im):
     cv.imshow("the image", im)
     key = cv.waitKey(0)
@@ -40,6 +56,17 @@ def resize(im):
     im = cv.resize(im, (0, 0), None, 0.5, 0.5)
     return im
 
+#Pass this the delta matrix constructed from only final matrix one
+#Use to compare to image one, they should be the same
+#Any diffferences are due to multiplying the colours by 1.02
+#Fiddle with those coefficients to find closest match
+#Ideally the whole system should just reconstruct the image
+def compare(delta):
+    im = cv.imread("im1.png")
+    im = im[:,960:,:] / 255
+    stacked = hstack_images(im, delta)
+    showim(stacked)
+############################################################
 
 #Test for colour 1
 
@@ -61,7 +88,6 @@ for i in range(0, unique_colours):
     #Colours in image don't perfectly match the colours in scale so just multiply by 1.02
     #to increase the range slightly
     mask1 = cv.inRange(im1, colours1[i] * 0.98, colours1[i] * 1.02)
-    print("found ", len(np.nonzero(mask1)[0]), " pixels")
     #Edit the final matrix for image one, put the value in at all pixel
     #locations of that colour
     final_matrix1[mask1 != 0] = vals[i]
@@ -69,19 +95,34 @@ for i in range(0, unique_colours):
     final_matrix2[mask2 != 0] = vals[i]
     #showim(hstack_images(mask1, mask2))
 
-#Here we should calculate they delta, just for testing i'm just using the final
-#matrix of image one
-value_delta_matrix = final_matrix1
+
+#In this function you should calculate the delta as you see fit
+#########################################
+def find_delta(matrix1, matrix2):
+    #Just leaving this empty for now
+    delta = matrix1
+    ##FOR EXAMPLE:
+    #delta = matrix2 - matrix2
+
+    return delta
+
+value_delta_matrix = find_delta(final_matrix1, final_matrix2)
+""" 
+max = np.max(value_delta_matrix)
+min = np.min(value_delta_matrix)
+all_delta_vals = np.unique(value_delta_matrix)
+all_delta_vals = np.sort(all_delta_vals)
+delta_vals = np.linspace(min, max, 32)
+"""
+
 #Delta matrix will be constructed below to contain colours
 delta_matrix = np.ndarray((1080, 960, 3))
-
 
 for i, v in enumerate(vals):
     #This should output an image composed of only the colours in the array colours1
     #There should be NO white pixels
-    delta_matrix[value_delta_matrix == v] = colours1[i]
+    #I am very stupid, values in image matrix must be normalised between 0 and 1
+    #MAX MAX MAX SUPER MAX MAX SUPER SUPER MAX
+    delta_matrix[value_delta_matrix == v] = colours1[i] / 255
 
 showim(resize(delta_matrix))
-   
-
-   
